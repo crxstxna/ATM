3f7219ed09d2e1c89bfd7305c9dca8615a045cbe
crxstxna <cristina.radulescu980@gmail.com>
2020-10-07 11:16:38 +0300

    Initial commit: basic structure.

---
+++ src/test/java/com/atmmachine/ATM/AtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmmachine.ATM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
